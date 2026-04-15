27894319e994fd68cd3b81a97f2d94e2190e2219
marciasouzam <156952820+marciasouzam@users.noreply.github.com>
2026-04-15 02:14:49 +0000

    Conclusão do serializer (eu acho)

--- servico_agendamentos/agendamentos/serializers.py
+++ servico_agendamentos/agendamentos/serializers.py
@@ -3,16 +3,44 @@ from rest_framework.reverse import reverse
 from .models import Usuario, Agendamento, Agenda
 
 class UsuarioSerializer(serializers.ModelSerializer):
+    _links = serializers.SerializerMethodField()
     class Meta:
         model = Usuario
-        fields = '__all__' #ALTERAR
+        fields = ['nome', 'email', 'telefone', 'cpf', 'data_nascimento', '_links']
+
+    def get__links(self, obj):
+        request = self.context.get('request')
+        return {
+            "self": reverse('usuario-detail', args=[obj.pk], request=request),
+        }
 
 class AgendaSerializer(serializers.ModelSerializer):
+    _links = serializers.SerializerMethodField()
     class Meta:
         model = Agenda
-        fields = '__all__' #ALTERAR
+        fields = ['medico_id', 'data', 'horario', 'disponivel', '_links']
+
+    def get__links(self, obj):
+        request = self.context.get('request')
+        return {
+            "self": reverse('agenda-detail', args=[obj.pk], request=request),
+        }
 
 class AgendamentoSerializer(serializers.ModelSerializer):
+    _links = serializers.SerializerMethodField()
+    
+    #para visualização dos dados já agendados 
+    usuario_detalhe = UsuarioSerializer(source='usuario', read_only=True)
+    agenda_detalhe = AgendaSerializer(source='agenda', read_only=True)
+
     class Meta:
         model = Agendamento
-        fields = '__all__' #ALTERAR
+        # 'usuario' e 'agenda' (IDs) para salvar
+        # 'usuario_detalhe' e 'agenda_detalhe' (Objetos) para ver
+        fields = ['id', 'usuario', 'agenda', 'usuario_detalhe', 'agenda_detalhe', '_links']
+
+    def get__links(self, obj):
+        request = self.context.get('request')
+        return {
+            "self": reverse('agendamento-detail', args=[obj.pk], request=request),
+        }
