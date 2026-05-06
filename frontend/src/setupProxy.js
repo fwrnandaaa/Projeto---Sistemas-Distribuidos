c93decf63482bb2e5fd70492a681f19992b046e9
marciasouzam <156952820+marciasouzam@users.noreply.github.com>
2026-05-06 21:39:55 +0000

    .

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -2,11 +2,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function (app) {
   app.use(
-    ['/medicos', '/especialidades'],
-    createProxyMiddleware({ target: 'http://localhost:8001', changeOrigin: true })
-  );
-  app.use(
-    ['/agendas', '/agendamentos'],
-    createProxyMiddleware({ target: 'http://localhost:8002', changeOrigin: true })
+    ['/medicos', '/especialidades', '/agendas', '/agendamentos'],
+    createProxyMiddleware({ target: 'http://localhost:8000', changeOrigin: true })
   );
 };
