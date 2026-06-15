36728efcbc13a74b5c3481b1c7c1dceb1869407c
fwrnandaaa <claudiazkn432@gmail.com>
2026-06-14 22:36:19 -0300

    mudança no cliente soap

--- soap_convenios/cliente/ClienteSOAP.java
+++ soap_convenios/cliente/ClienteSOAP.java
@@ -3,7 +3,6 @@ import java.io.OutputStream;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.util.Scanner;
-import java.util.regex.*;
 
 public class ClienteSOAP {
 
@@ -31,42 +30,18 @@ public class ClienteSOAP {
         return resposta;
     }
 
-   static String extrairResultado(String xml) {
-    // Tenta extrair qualquer tag Result
-    Pattern p = Pattern.compile("<tns:[^>]*Result>(.*?)</tns:[^>]*Result>", Pattern.DOTALL);
-    Matcher m = p.matcher(xml);
-    if (m.find()) return m.group(1);
-
-    // Extrai lista de planos com namespace tns:string
-    Pattern ps = Pattern.compile("<tns:string>(.*?)</tns:string>");
-    Matcher ms = ps.matcher(xml);
-    StringBuilder sb = new StringBuilder();
-    while (ms.find()) {
-        if (sb.length() > 0) sb.append(", ");
-        sb.append(ms.group(1));
-    }
-    if (sb.length() > 0) return sb.toString();
-
-    // Fault
-    Pattern pf = Pattern.compile("<faultstring[^>]*>(.*?)</faultstring>", Pattern.DOTALL);
-    Matcher mf = pf.matcher(xml);
-    if (mf.find()) return "ERRO: " + mf.group(1).trim();
-
-    return "Sem resposta";
-}
-
     public static void main(String[] args) throws Exception {
 
-        System.out.println(" Listando planos aceitos ");
+        System.out.println("=== Listando planos aceitos ===");
         String xmlListar = "<soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:tns=\"clinica.convenio\"><soapenv:Body><tns:listar_planos_aceitos/></soapenv:Body></soapenv:Envelope>";
-        System.out.println(extrairResultado(chamarSOAP(xmlListar)));
+        System.out.println(chamarSOAP(xmlListar));
 
-        System.out.println("\n Cadastrando convênio ");
+        System.out.println("\n=== Cadastrando convênio ===");
         String xmlCadastrar = "<soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:tns=\"clinica.convenio\"><soapenv:Body><tns:cadastrar_convenio><tns:cpf>999.888.777-66</tns:cpf><tns:plano>Amil</tns:plano></tns:cadastrar_convenio></soapenv:Body></soapenv:Envelope>";
-        System.out.println(extrairResultado(chamarSOAP(xmlCadastrar)));
+        System.out.println(chamarSOAP(xmlCadastrar));
 
-        System.out.println("\n Verificando convênio válido ");
+        System.out.println("\n=== Verificando convênio válido ===");
         String xmlVerificar = "<soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:tns=\"clinica.convenio\"><soapenv:Body><tns:verificar_convenio><tns:cpf>999.888.777-66</tns:cpf><tns:plano>Amil</tns:plano></tns:verificar_convenio></soapenv:Body></soapenv:Envelope>";
-        System.out.println(extrairResultado(chamarSOAP(xmlVerificar)));
+        System.out.println(chamarSOAP(xmlVerificar));
     }
 }
